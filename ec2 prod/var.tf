09b327b757088caa5c306e6f50da1a090a26dae7
muralidhar <38804803+satyamuralidhar@users.noreply.github.com>
2019-08-06 14:02:20 +0530

    Create var.tf

---
+++ ec2 prod/var.tf
@@ -0,0 +1,29 @@
+
+variable "access" {
+
+}
+variable "secret" {
+
+}
+
+
+variable "ami" {
+
+  default = "ami-0d7hjbjhbb2bb1c1"
+
+}
+
+variable "key_name" {
+
+  default = "ansible"
+
+}
+
+
+
+variable "security" {
+
+  default = ["sec"]
+
+}
+
